76aebdef2466ba3997cda48401e044be1f3a5c93
finanovita <finanovita479@gmail.com>
2024-05-30 12:00:26 +0700

    kondisi if dan else mengecek antrian kosng

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -25,12 +25,66 @@ public:
             return;
         }
 
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            //jika REAR berapa di posisi terakhir array, kembali ke awla array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+
     }
 
-
+   
 };
 
 int main()
 {
-    
-}
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+            case '1': {
+
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.Remove();
+                break;
+            }
+            case '3': {
+                q.Display();
+                break;
+            }
+            case '4': {
+                return 0;
+            }
+            default: {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+};
