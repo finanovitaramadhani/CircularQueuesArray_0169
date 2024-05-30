e9cef5b1937fd43086d707b0a27dadfb6592d862
finanovita <finanovita479@gmail.com>
2024-05-30 13:16:12 +0700

    Iterasi dari awal array hingga REAR

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -95,6 +95,11 @@ public:
 
         FRONT_Position = 0;
 
+        //Iterasi dari awal array hingga REAR
+        while (FRONT_Position <= REAR_Position) {
+            cout << queue_array[FRONT_Position] << " ";
+            FRONT_Position++;
+        }
       
     }
    
